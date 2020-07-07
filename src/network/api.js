5cdfe78ed43884d0d8aef1a0ed0b7b1a1a210492
SeaWaveW <1170876169@qq.com>
2020-07-07 17:47:14 +0800

    实现了添加、编辑功能

--- src/network/api.js
+++ src/network/api.js
@@ -20,11 +20,39 @@ export const reqMenus = (()=>{
     })
 })
 
-//用户列表的请求
+//用户列表的请求 与 关键字搜索
 export const reqUsersList = ((params)=>{
     return request({
         url:"users",
         method:"get",
         params
+    })
+})
+
+//用户状态修改
+export const reqUpdateUsersStatus = ((uId,type)=>{
+    return request({
+        url:`users/${uId}/state/${type}`,
+        method:"put"
+    })
+})
+
+//添加用户
+export const reqAddUsers = ((userForm)=>{
+    return request({
+        url:"users",
+        method:"post",
+        data:userForm
+    })
+})
+
+//修改用户
+export const reqExitUsers = ((id,email,mobile)=>{
+    return request({
+        url:`users/${id}`,
+        method:"put",
+        data:{
+            email,mobile
+        }
     })
 })
--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -7,12 +7,12 @@
         <!-- 搜索栏 -->
         <el-row :gutter="30">
             <el-col :span="8">
-                <el-input placeholder="请输入内容" v-model="searchInput" class="input-with-select">
-                    <el-button slot="append" icon="el-icon-search"></el-button>
+                <el-input placeholder="请输入内容" v-model="params.query" class="input-with-select"  clearable @clear="searchClearClick">
+                    <el-button slot="append" icon="el-icon-search" @click="searchUsers"></el-button>
                 </el-input>
             </el-col>
             <el-col :span="3">
-                <el-button type="primary">添加用户</el-button>
+                <el-button type="primary" @click="AddUserClick">添加用户</el-button>
             </el-col>
         </el-row>
         <!-- 数据表格 -->
@@ -28,13 +28,13 @@
             <el-table-column prop="role_name" label="角色" align="center"></el-table-column>
             <el-table-column prop="mg_state" label="状态" align="center">
                 <template slot-scope="data">
-                    <el-switch v-model="data.row.mg_state"></el-switch>
+                    <el-switch v-model="data.row.mg_state" @change="changeUsersClick(data.row)"></el-switch>
                 </template>
             </el-table-column>
             <el-table-column label="操作" align="center">
                 <template slot-scope="data">
                     <el-tooltip class="item" effect="dark" content="编辑" placement="top" :enterable="false" >
-                        <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+                        <el-button type="primary" icon="el-icon-edit" size="mini" @click="ExidUserClick(data.row)"></el-button>
                     </el-tooltip>
                     <el-tooltip class="item" effect="dark" content="删除" placement="top" :enterable="false">
                         <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
@@ -58,33 +58,43 @@
         </el-pagination>
     </el-card>
     
+    <AddExidUsers ref="addexidusers" :userInfoData="userInfoData" :aeuClose="aeuClose" @updateUsersList="updateUsersList"></AddExidUsers>
+
 </div>
 </template>
     
 <script>
 import HeaderAdmin from "components/HeaderAdmin.vue"
+import {reqUsersList,reqUpdateUsersStatus} from 'network/api';
+// import AddExidUsers from "users/child/AddExidUsers.vue"
+import AddExidUsers from "./child/AddExidUsers.vue"
 
-import {reqUsersList} from 'network/api';
 
 export default {
     name:"Users",
     components:{
-        HeaderAdmin
+        HeaderAdmin,AddExidUsers
     },
     data(){return{
-        searchInput:"",//搜索栏的input框数据
         //getUsers请求
         params:{
+            query:"",//搜索关键字-搜索栏的input框数据
             pagenum:1,//第几页
             pagesize:2,//每页多少条
         },
         //getUsers请求响应的数据替换掉表格中的数据
         usersData:[],
         //总页数
-        total:0
+        total:0,
+        //传给弹窗的数据
+        userInfoData:{}
     }},
+    created(){
+        //页面一创建就调用用户列表请求
+        this.getUsers()
+    },
     methods:{
-        //用户列表请求
+        //用户列表请求--------------------------------------------------------------------------------------------------
         async getUsers(){
             const result = await reqUsersList(this.params)
             //判断是否请求成功
@@ -100,19 +110,52 @@ export default {
             this.usersData = users
             this.total = total
         },
-        //分页
-        handleSizeChange(value){
-            this.params.pagesize = value
+        //分页--------------------------------------------------------------------------------------------------
+        handleSizeChange(size){
+            this.params.pagesize = size
             this.getUsers()
         },
-         handleCurrentChange(value) {
-            this.params.pagenum = value
+         handleCurrentChange(page) {
+            this.params.pagenum = page
             this.getUsers()
+        },
+        //搜索--------------------------------------------------------------------------------------------------
+        searchUsers(){
+            this.params.pagenum = 1 //从一页开始
+            this.getUsers()
+        },
+        //搜索框清空还原
+        searchClearClick(){
+            this.params.pagenum = 1
+            this.getUsers()
+        },
+        //添加用户事件--------------------------------------------------------------------------------------------------
+        AddUserClick(){
+            this.$refs.addexidusers.dialogVisible = true
+            this.userInfoData = {}
+        },
+        //用户状态----------------------------------------------------------------------------
+        async changeUsersClick(userData){
+            const {id,mg_state} = userData
+            const result = await reqUpdateUsersStatus(id,mg_state)
+            if(result.meta.status != 200) return this.$message.error(result.meta.msg) //设置不成功
+            this.$message.success(result.meta.msg)
+        },
+        //编辑用户事件--------------------------------------------------------------------------------------------------
+        ExidUserClick(userData){
+            this.userInfoData = userData
+            this.$refs.addexidusers.dialogVisible = true
+        },
+        //添加、编辑按钮事件--bug解决
+        aeuClose(){
+            this.userInfoData = {}
+        },
+        //接收孩子传归来的事件updateUsersList
+        updateUsersList(){
+            this.getUsers() //更新列表
         }
     },
-    created(){
-        this.getUsers()
-    }
+    
 }
     
 </script>
